7cba21eb58f6f63d983b4f1de0ff135a234c03ae
Kevin Murray <k.d.murray.91@gmail.com>
2013-02-01 19:06:37 +1100

    crd.py finished, fully pep8, commented and docstringed

--- exp_design/crd.py
+++ exp_design/crd.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
-
 import csv
 import argparse
 import os.path
 from random import shuffle
 from copy import deepcopy
 
+
 def get_args():
+    """Get program command line arguments"""
     parser = argparse.ArgumentParser(description="Create a Completly"
             "Randomised Design experimental layout from a CSV file")
-    parser.add_argument("-i", "--input", action="store", dest="input")
+    parser.add_argument("-i", "--input", action="store", dest="input",
+            help="Input file path, must exist")
     parser.add_argument("-o", "--output", action="store", required=True,
-            dest="output")
+            dest="output", help="Output file path.")
     parser.add_argument("-t", "--tray-dimensions", action="store",
-            required=True, dest="tray")
+            required=True, dest="tray", help="The dimensions of the tray,"
+            " given as <rows>x<cols> (note that's a lower case letter x)")
     parser.add_argument("-r", "--replicates", action="store", default=1,
-            required=True, dest="reps", type=int)
+            required=True, dest="reps", type=int, help="The number of"
+            " replicates per plant and condition")
     parser.add_argument("-c", "--create", action="store_true", default=False,
-            dest="create")
+            dest="create", help="Flag, creates a pro-forma CSV to enter"
+            " conditions and plants into")
     return parser.parse_args()
+
 
 def create_csv():
     """Writes a pro-forma csv to be filled in for input"""
@@ -27,38 +33,45 @@ def create_csv():
     out_fh.write('"Condition","Plant"')
     out_fh.close()
 
+
 def get_tray_positions(tray_x, tray_y):
+    """Gets a list of tray positions from some dimensions.
+    >>> get_tray_positions(2,3)
+    ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+    """
     positions = []
     for yyy in xrange(tray_y):
         y_str = ""
-        # This generates the y-string backwards (from right to left)
-        while yyy > 25:
-            lhs = yyy // 26
-            rhs = yyy % (lhs * 26)
+        # This generates the y-string backwards (from right to left), hence
+        # y_str = str + y_str, not just y_str += str
+        while yyy >= 26:  # Note the >=
+            lhs = yyy // 26  # the most signficant character(s)
+            rhs = yyy % (lhs * 26)  # the least significant (single) char
             y_str = chr(rhs + 65) + y_str
             yyy = lhs - 1  # We expect yyy to be 0-indexed, lhs is 1-indexed
         y_str = chr(yyy + 65) + y_str
         for xxx in xrange(tray_x):
-            positions.append(y_str + str(xxx+1))  # xxx 0-index, want 1-index
+            positions.append(y_str + str(xxx + 1))  # xxx 0index, want 1index
     return positions
 
 
 def main():
+    """Main program logic"""
     args = get_args()
 
+    # Create experiment proforma and exit, if requested
     if args.create:
         create_csv()
         exit()
-    
+
     # Check input file exist
     if not os.path.isfile(args.input):
         ValueError("Input file must exist: %s" % args.input)
-        
+
     in_file = open(args.input, "rt")
     in_csv = csv.DictReader(in_file)
     out_file = open(args.output, "wt")
     out_cols = ["Condition", "Tray", "Position", "Plant"]
-    #out_cols = ["Condition", "Tray", "Position", "Replicate", "Plant"]
     out_csv = csv.DictWriter(out_file, out_cols)
     out_csv.writeheader()
 
@@ -67,42 +80,65 @@ def main():
     tray_pos = get_tray_positions(*tray_dim)
     tray_size = len(tray_pos)
 
-
     for line in in_csv:
         if line["Condition"] not in plant_dict:
             plant_dict[line["Condition"]] = []
         plant_dict[line["Condition"]].append(line["Plant"])
 
     tray_count = 0
+    total_plant_count = 0
+    condition_count = 0
+
+    # For each condition, create tray(s) of it's plants
     for condition, plants in plant_dict.iteritems():
+        condition_count += 1
+
+        # Get a list of all the plants for this condition
         if args.reps > 1:
             these_plants = []
             for rrr in xrange(args.reps):
                 for plant in plants:
-                    these_plants.append("%s R%i" %(plant, rrr + 1))
+                    these_plants.append("%s R%i" % (plant, rrr + 1))
         else:
             these_plants = plants
+
         tray_count += 1
+        # Must deepcopy, otherwise we remove positions when we pop(0) below
         this_tray_pos = deepcopy(tray_pos)
         plant_count = 0
+
+        # Randomise order, 1000 shuffles is really quick and should be random
+        # enough
         for iii in xrange(1000):
             shuffle(these_plants)
+
         for plant in these_plants:
+            total_plant_count += 1
+            plant_count += 1
+
+            # Create new tray if the current one is full
             if plant_count > tray_size:
+                # Must deepcopy, see above
                 this_tray_pos = deepcopy(tray_pos)
                 tray_count += 1
                 plant_count = 0
-            plant_count += 1
+
+            # Form and write the row
             row = {
                     "Condition": condition,
                     "Tray": tray_count,
                     "Position": this_tray_pos.pop(0),
-#                    "Replicate": rrr + 1,
                     "Plant": plant
                     }
             out_csv.writerow(row)
     out_file.close()
     in_file.close()
+
+    # Print a summary
+    print("Done!")
+    print("Processed %i replicates of %i experimental conditions" %
+            (args.reps, condition_count))
+    print("Which gives a total of %i plants" % total_plant_count)
 
 
 if __name__ == "__main__":
