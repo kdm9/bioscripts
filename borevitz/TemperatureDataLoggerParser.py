32bf01b123f15f748f0d9dbd8140beda900c8f7b
Kevin Murray <k.d.murray.91@gmail.com>
2013-02-23 18:51:26 +1100

    TemperatureDataLoggerParser.py seems done

---
+++ borevitz/TemperatureDataLoggerParser.py
@@ -0,0 +1,27 @@
+import re
+import sys
+from datetime import datetime
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+in_fh = open(infile)
+out_fh = open(outfile, "w")
+out_fh.write("'ISO Date','Temperature','Humidity'")
+
+at_start = False
+for line in in_fh:
+    while not line.startswith("-") and not at_start:
+        line = next(in_fh)
+    while not line.find("%RH") > 0:
+        line = next(in_fh)
+    at_start = True
+    fields = line.strip().split()
+    date = datetime.strptime(fields[5], "%d-%m-%y/%H:%M:%S")
+    temp = float(fields[1])
+    rh = float(fields[3])
+    csv_line = "'%s',%f,%f" % (date.isoformat(), temp, rh)
+    out_fh.write(csv_line)
+
+in_fh.close()
+out_fh.close()
