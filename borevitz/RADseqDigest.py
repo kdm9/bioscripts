7f46977be1b651501de238b55d7403dc71d18eec
Kevin Murray <k.d.murray.91@gmail.com>
2013-03-06 17:35:04 +1100

    fixed formatting and comments/docstrings in RADseqDigest.py

--- borevitz/RADseqDigest.py
+++ borevitz/RADseqDigest.py
@@ -3,64 +3,69 @@ from Bio import SeqIO
 import optparse
 import sys
 
-# Get all 
+# Get all enzymes supported by the Bio.Retriction module
 enzymes = Restriction.Restriction_Dictionary.rest_dict.keys()
 
 def get_options():
     """
     This function allows commandline arguments to be passed to the script, so
-    that you dont need to edit it every time you want to use a different 
+    that you dont need to edit it every time you want to use a different set of
+    settings.
     """
     parser = optparse.OptionParser('usage: %prog [options] ')
     parser.add_option(
-        '-s',
-        '--seqfile',
-        dest='seqfile',
-        help='File containing sequence to be analysed, as FASTA',
-        metavar='FILE',
-        default='')
+            '-s',
+            '--seqfile',
+            dest='seqfile',
+            help='File containing sequence to be analysed, as FASTA',
+            metavar='FILE',
+            default=''
+            )
     parser.add_option(
-        '-e',
-        '--enzyme',
-        dest='enzyme',
-        help='Enzyme',
-        default="EcoRI")
-    parser.add_option('-l',
-        '--list-enzymes',
-        dest='listenzymes',
-        help='print list of enzymes',
-        action="store_true",
-        default=False)
+            '-e',
+            '--enzyme',
+            dest='enzyme',
+            help='Enzyme',
+            default="EcoRI"
+            )
     parser.add_option(
-        '-c',
-        "--count",
-        dest="count",
-        help="count all restriction sites, equivalent to -m 0 -x infinity",
-        action="store_true",
-        default=False
-        )
+            '-l',
+            '--list-enzymes',
+            dest='listenzymes',
+            help='print list of enzymes',
+            action="store_true",
+            default=False
+            )
     parser.add_option(
-        '-m',
-        "--min-length",
-        dest="minlen",
-        help="minimum length between restriction sites",
-        type=int,
-        default=200
-        )
+            '-c',
+            "--count",
+            dest="count",
+            help="count all restriction sites, equivalent to -m 0 -x infinity",
+            action="store_true",
+            default=False
+            )
     parser.add_option(
-        '-x',
-        "--max-length",
-        dest="maxlen",
-        help="maximum length between restriction sites",
-        type=int,
-        default=600
-        )
+            '-m',
+            "--min-length",
+            dest="minlen",
+            help="minimum length between restriction sites",
+            type=int,
+            default=200
+            )
     parser.add_option(
-        '-v',
-        "--verbose",
-        dest="verbose",
-        action="count"
-        )
+            '-x',
+            "--max-length",
+            dest="maxlen",
+            help="maximum length between restriction sites",
+            type=int,
+            default=600
+            )
+    parser.add_option(
+            '-v',
+            "--verbose",
+            dest="verbose",
+            action="count"
+            )
     options, args = parser.parse_args()
     die = False
     if options.enzyme not in Restriction.Restriction_Dictionary.rest_dict.keys():
@@ -69,7 +74,7 @@ def get_options():
     if not options.seqfile and not options.listenzymes:
         print "ERROR: seqfile is required"
         die = True
-    
+
     # If there's something wrong with the options
     if die:
         parser.print_help()
@@ -88,7 +93,7 @@ if opts.listenzymes:
     sys.exit(0)
 
 # gets enzyme class by name
-cutter = getattr(Restriction, opts.enzyme)  
+cutter = getattr(Restriction, opts.enzyme)
 print cutter
 print cutter.site
 # Opens file, and creates fasta reader
@@ -98,26 +103,26 @@ seqs = SeqIO.parse(seq_file, "fasta")
 # Digest all sequences in the fasta file
 count = 0
 for record in seqs:
-    record_count = 0 
+    record_count = 0
     # When we're counting, we only want to show how many cut sites there
     # are. Given that cutting a sequence 0 times gives one fragment, we
     # need to decrement this, or we add one to the true number of sites
 
     # Do virtual digest
     fragments = cutter.catalyse(record.seq)
-    
+
     # Find fragment lenghts
     fragment_lengths = []
     for seq in fragments:
         fragment_lengths.append(len(seq))
 
-    # Count how many 
+    # Count how many fragments meet the specified selection criteria
     for length in fragment_lengths:
-        if opts.count:
+        if opts.count:  # Count everything
             record_count += max(0, len(fragment_lengths) - 1)
         elif length > opts.minlen and  length < opts.maxlen:
             record_count += 1
-    
+
     # Append the counts for this record to the total sum
     count += record_count
 
