a8a6c01152635eafbda16bc71298f13d5f57c975
Kevin Murray <k.d.murray.91@gmail.com>
2013-08-13 18:17:19 +1000

    fix bug in gff_make_intergenes
    prints to file now if -o is given
    also fixed the overlapping genes bug

--- fileconversion/gff_make_intergenes.py
+++ fileconversion/gff_make_intergenes.py
@@ -19,6 +19,9 @@ def _print_intergene(chrom, start, stop, name):
 
 def main():
     opts = docopt(__doc__)
+    if opts["-o"]:
+        if os.path.exists(os.dirname(opts["-o"])):
+            sys.stdout = open(opts["-o"])
     gff_fn = opts["<infile>"]
     db_fn = gff_fn + ".db"
 
@@ -29,8 +32,8 @@ def main():
 
     assert "gene" in list(fdb.featuretypes()), "Database doesn't contain genes"
     for chrom in fdb.chromosomes():
-        last_stop = 0
-        last_id = chrom + "_START"
+        biggest_stop = 0
+        biggest_stop_id = chrom + "_START"
         genes = list(fdb.features_of_type("gene", chrom=chrom))
         genes.extend(list(fdb.features_of_type("transposable_element",
                 chrom=chrom)))
@@ -44,19 +47,22 @@ def main():
             stop = gene.stop
             agi = gene.id
 
-            if start > last_stop:
-                ig_name = last_id + "-" + agi
-                ig_start = last_stop + 1  # starts at the next pos
+            if start > biggest_stop:
+                ig_name = biggest_stop_id + "-" + agi
+                ig_start = biggest_stop + 1  # starts at the next pos
                 ig_stop = start - 1  # stops before the next gene
                 _print_intergene(chrom, ig_start, ig_stop, ig_name)
+            if stop > biggest_stop:
+                biggest_stop = stop
+                biggest_stop_id = agi
             print(gene)
             for transcript in fdb.children(gene):
                 print(transcript)
                 for exon in fdb.children(transcript):
                     print(exon)
-            last_stop = stop
-            last_id = agi
 
+    if opts["-o"]:
+        sys.stdout.close()
 
 if __name__ == "__main__":
     main()
