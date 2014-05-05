d75f7e0c827cd2dc56812721727978bf176bad5b
Kevin Murray <spam@kdmurray.id.au>
2014-05-05 14:35:59 +1000

    [sc/offsetSC.py] add format CLI param

--- solarcalc/offsetSolarcalcDates.py
+++ solarcalc/offsetSolarcalcDates.py
@@ -5,19 +5,19 @@ from datetime import datetime, timedelta
 
 __doc__ = """
 USAGE:
-    script.py [ -i INFILE -o OUTFILE ] -d DAYS
+    script.py [ -i INFILE -o OUTFILE -f FMT] -d DAYS
 
 OPTIONS:
     -d DAYS     Integral number of days to offset file by. Negative for
                     backwards in time, positive for forwards in time.
+    -f FMT      Date format. [Default: %m/%d/%Y]
     -i INFILE   Input solarcalc CSV file. [stdin]
     -o OUTFILE  Output solarcalc CSV file. [stdout]
 """
 
-
 def main():
+    # option parsing
     opts = docopt(__doc__)
-
     try:
         if int(opts["-d"]) != 0:
             offset = int(opts["-d"])
@@ -25,7 +25,6 @@ def main():
             raise ValueError
     except ValueError:
         sys.stderr.write("ERROR: value for -d must be a non-zero number\n")
-
     if "-i" in opts and opts["-i"]:
         in_fh = open(opts["-i"])
     else:
@@ -34,23 +33,25 @@ def main():
         out_fh = open(opts["-o"], "w")
     else:
         out_fh = sys.stdout
-
+    # set date format to either default or what was on the CLI
+    date_fmt = opts["-f"]
+    # Keys for csv.DWtr
     csv_keys = [
-            "Date", "Time", "ChamTemp", "ChamRH", "LED1", "LED2", "LED3", 
+            "Date", "Time", "ChamTemp", "ChamRH", "LED1", "LED2", "LED3",
             "LED4", "LED5", "LED6", "LED7", "Total Solar (Watt/m2)",
             "Simulated Date-Time "
             ]
-
+    # set up files
     reader = DictReader(in_fh)
     writer = DictWriter(out_fh, csv_keys)
     writer.writeheader()
-
+    # main loop
     for line in reader:
-        date = datetime.strptime(line["Date"], "%m/%d/%Y")
+        date = datetime.strptime(line["Date"], date_fmt)
         newdate = date + timedelta(offset)
-        line["Date"] = newdate.strftime("%m/%d/%Y")
+        line["Date"] = newdate.strftime(date_fmt)
         writer.writerow(line)
-
+    # clean up files
     in_fh.close()
     out_fh.close()
 
