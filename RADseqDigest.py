044e43a476e64ae74ea59a000c5fd3228d222d66
Kevin Murray <spam@kdmurray.id.au>
2014-10-29 11:44:45 +1100

    Move useful stuff to ./

--- RADseqDigest.py
+++ RADseqDigest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from Bio import Restriction
 from Bio import SeqIO
 import optparse
@@ -74,65 +75,67 @@ def get_options():
     if not options.seqfile and not options.listenzymes:
         print "ERROR: seqfile is required"
         die = True
-
     # If there's something wrong with the options
     if die:
         parser.print_help()
         sys.exit(1)
     return options
 
+def main(opts):
+    """Does the actual digestions given an option namespace"""
+    # If we've been asked to list all enzymes, do this now and exit
+    if opts.listenzymes:
+        print "The following enzymes are supported"
+        for enzyme in enzymes:
+            print enzyme
+        sys.exit(0)
 
-# Store Commandline args
-opts = get_options()
+    # gets enzyme class by name
+    cutters = opts.enzyme.split(",")
+    cutters = [getattr(Restriction, enz) for enz in cutters]
+    print("Using the following exzymes:")
+    for cutter in cutters:
+        print("\t{}".format(cutter))
+        print("\t{}".format(cutter.site))
 
-# If we've been asked to list all enzymes, do this now and exit
-if opts.listenzymes:
-    print "The following enzymes are supported"
-    for enzyme in enzymes:
-        print enzyme
-    sys.exit(0)
+    # Opens file, and creates fasta reader
+    seq_file = open(opts.seqfile, "rb")
+    seqs = SeqIO.parse(seq_file, "fasta")
 
-# gets enzyme class by name
-cutters = opts.enzyme.split(",")
-cutters = [getattr(Restriction, enz) for enz in cutters]
-print("Using the following exzymes:")
-for cutter in cutters:
-    print("\t{}".format(cutter))
-    print("\t{}".format(cutter.site))
+    # Digest all sequences in the fasta file
+    count = 0
+    for record in seqs:
+        record_count = 0
+        # When we're counting, we only want to show how many cut sites there
+        # are. Given that cutting a sequence 0 times gives one fragment, we
+        # need to decrement this, or we add one to the true number of sites
 
-# Opens file, and creates fasta reader
-seq_file = open(opts.seqfile, "rb")
-seqs = SeqIO.parse(seq_file, "fasta")
+        # Do virtual digest
+        # TODO: change this to use `cutters` and RestrictionBatch
+        fragments = cutter.catalyse(record.seq)
 
-# Digest all sequences in the fasta file
-count = 0
-for record in seqs:
-    record_count = 0
-    # When we're counting, we only want to show how many cut sites there
-    # are. Given that cutting a sequence 0 times gives one fragment, we
-    # need to decrement this, or we add one to the true number of sites
+        # Find fragment lenghts
+        fragment_lengths = []
+        for seq in fragments:
+            fragment_lengths.append(len(seq))
 
-    # Do virtual digest
-    # TODO: change this to use `cutters` and RestrictionBatch
-    fragments = cutter.catalyse(record.seq)
+        # Count how many fragments meet the specified selection criteria
+        for length in fragment_lengths:
+            if opts.count:  # Count everything
+                record_count += max(0, len(fragment_lengths) - 1)
+            elif length > opts.minlen and  length < opts.maxlen:
+                record_count += 1
 
-    # Find fragment lenghts
-    fragment_lengths = []
-    for seq in fragments:
-        fragment_lengths.append(len(seq))
+        # Append the counts for this record to the total sum
+        count += record_count
 
-    # Count how many fragments meet the specified selection criteria
-    for length in fragment_lengths:
-        if opts.count:  # Count everything
-            record_count += max(0, len(fragment_lengths) - 1)
-        elif length > opts.minlen and  length < opts.maxlen:
-            record_count += 1
+        if opts.verbose > 0:
+            # Print summary
+            print "%s has %i RADseq tags" % (record.id, record_count)
 
-    # Append the counts for this record to the total sum
-    count += record_count
+    print "In total, %i RADseq tags were found" % count
 
-    if opts.verbose > 0:
-        # Print summary
-        print "%s has %i RADseq tags" % (record.id, record_count)
-
-print "In total, %i RADseq tags were found" % count
+if __name__ == "__main__":
+    # Store Commandline args
+    opts = get_options()
+    main(opts)
